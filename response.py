296ee380e4f618c884ec6c32de167188691a20ac
Qamarrul Danial <q.qamarrul@gmail.com>
2025-02-16 15:01:16 +0800

    New dictionary file added
    Created new file (locations.py) to store the dictionary of towns and it's places.
    Locations.py contains the same places as the previous version from response.py only now it's stored in a dedicated file and each place is grouped into 1 out of 3 categories (parks, malls, food).
    
    locations.py is then imported to response.py leading to its same functionality.
    
    No change in main.py.

--- response.py
+++ response.py
@@ -1,4 +1,5 @@
 from random import choice
+from locations import locations # Import dictionary from locations.py
 
 # Pre-generated message list
 Reply_List = [
@@ -7,7 +8,7 @@ Reply_List = [
     "{} could be fun spot to rest and relax!",
     "I heard {} is a great place to go!",
     "Maybe try {}? It's a popular choice for some people!",
-    "If you're looking for something to just lepak, {} is one of recommended picks!",
+    "If you're looking for something to just lepak, {} is one of the recommended picks!",
     "{} sounds like a cozy place to just chill!",
     "I would suggest going to {}!",
     "{} is a good spot!",
@@ -23,44 +24,13 @@ def get_response(user_input: str) -> str:
     elif lowered == 'lepaksg':
         return 'Hi there! Which town are you planning to lepak at?'
 
-    # Dictionary for recommended spots
-    Recommended_Spots = {
-    'ang mo kio': ['Brew & Co.', 'Ang Mo Kio Park', 'AMK Hub'],
-    'bedok': ['Bedok Mall', 'Bedok Jetty', 'Bedok Maze'],
-    'bishan': ['Windsor Nature Park', 'Thomson Plaza', 'Kings Cart Coffee'],
-    'bugis': ['Kampong Glam Cafe', 'Rumi', 'Chix Hot Chicken'],
-    'bukit batok': ['Bukit Batok Nature Park', 'Coexist Coffee Co', 'West Mall'],
-    'bukit merah': ['Vivo City', 'Henderson Wave', 'Labrador Nature Reserve'],
-    'bukit panjang':['Junction 10', 'Bukit Timah Nature Reserve', 'Hillion Mall'],
-    'choa chu kang': ['Lot One', 'SAFRA Bowling', 'Teck Whye Garden'],
-    'clementi': ['Clementi Mall', 'West Coast Park', 'Burnt Cones'],
-    'west coast':['Clementi Mall', 'West Coast Park', 'Burnt Cones'],
-    'geylang': ['Geylang Serai', 'Joo Chiat Complex', 'Paya Lebar Square', '89.7 Supper Club'],
-    'hougang': ['Hougang Mall', 'Maplewood Park', 'Lola\'s Cafe'],
-    'jurong east': ['Jem/Westgate', 'Padi Emas', 'Imm'],
-    'jurong west': ['Jurong Point', 'Lakeside', 'Al-Ayza'],
-    'kallang': ['Stadium', 'Decathlon', 'City Square Mall'],
-    'whampoa': ['Stadium', 'Decathlon', 'City Square Mall'],
-    'pasir ris': ['Pasir Ris Park', 'White Sands Mall, Downtown East', '89.7 Supper Club'],
-    'punggol': ['Punggol Beach', 'Tebing Lane', 'Hai Bin'],
-    'queenstown': ['Queensway', 'ABC', 'Star Vista'],
-    'sembawang': ['Sun Plaza', 'Sembawang Park', 'Canberra Plaza'],
-    'sengkang': ['Seletar Mall', 'Compass One', 'Rivervale Mall'],
-    'serangoon': ['Nex', 'Coney Island Park', 'Nim Meadow Park'],
-    'tampines': ['Tampines Mall', 'Changi City Point', 'Tampines Hub'],
-    'tengah': ['Plantation Plaza', 'Garden Vines', 'Le Quest Shopping Mall'],
-    'toa payoh': ['Toa Payoh Town Park', 'Creamier', 'Woodleigh Mall'],
-    'woodlands': ['Causewaypoint', 'Woodlands Waterfront Park', 'Al-Ameen', 'Citrus By the Pool'],
-    'yishun': ['Northpoint City', 'Junction Nine', 'Wisteria Mall'],
-    'bukit timah': ['Al-Azhar Restaurant', 'Bukit Timah Plaza', 'Atlas Coffeehouse'],
-    'marine parade': ['Parkway Parade', 'East Coast Park', 'Cafe Melba'],
-    'central area': ['Somerset', 'MBS', 'Funan']
-    } 
+
 
     # Check if user input contains any known towns
-    for location, places in Recommended_Spots.items():
-        if location in lowered:
-            place = choice(places) # A spot is chosen at random depending on the selected town
+    for town, categories in locations.items():
+        if town in lowered:
+            category = choice(list(categories.keys())) # A spot is chosen at random depending on the selected town
+            place = choice(categories[category])
             message = choice(Reply_List).format(place) # Format with a pre-written reply
             return message
     
